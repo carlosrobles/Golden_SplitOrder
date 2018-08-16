d24d2f573962d985ec2a2cd46a55c0062e52bd20
Ilyas Soladogun <liyassoladogun@Nidhis-MacBook-Pro.local>
2018-08-16 08:54:21 +0400

    optimization & refactoring

--- code/local/GoldenScent/SplitOrder/Model/Observer.php
+++ code/local/GoldenScent/SplitOrder/Model/Observer.php
@@ -1,17 +1,21 @@
 <?php
 class GoldenScent_SplitOrder_Model_Observer
 {
+    const BRAND_IN_ORDER  = 1;
+    const OBSERVER_HAS_RUN  = 'my_observer_has_run';
+
     public function SplitOrder(Varien_Event_Observer $observer)
     {
-        if (Mage::registry('my_observer_has_run')) {
-            Mage::log('Mstop', null, 'mylogfile.log');
-            return true;
+        if (Mage::registry(GoldenScent_SplitOrder_Model_Observer::OBSERVER_HAS_RUN))
+        {
+            Mage::log('Observer has ran', null, 'GoldenScent_SplitOrder.log');
+            return;
         }
 
         $order = $observer->getEvent()->getOrder();
-        //Mage::log($order->getShippingMethod(), null, 'mylogfile.log');
         $allItem = $order->getAllVisibleItems();
-        foreach ($allItem as $item) {
+        foreach ($allItem as $item)
+        {
             $product = $item->getProduct();
             $_product = Mage::getModel('catalog/product')->load($product->getId());
             $brand_Item = $_product->getData("brand");
@@ -21,75 +25,93 @@ class GoldenScent_SplitOrder_Model_Observer
             }
 
         }
-        Mage::log($brand, null, 'mylogfile.log');
-        $productArray= array();
-        foreach ($brand as $key => $val) {
-            $productArray[$val][] = $key;
+        $brandProductArray= array();
+        foreach ($brand as $key => $val)
+        {
+            $brandProductArray[$val][] = $key;
         }
-        if (count($productArray) > 1) {
-          $this->createOrder($order, $productArray, $itemQty);
+        if (count($brandProductArray) > GoldenScent_SplitOrder_Model_Observer::BRAND_IN_ORDER) {
+          $this->createOrder($order, $brandProductArray, $itemQty);
         }
     }
 
-    public function createOrder($order, $array, $itemQty)
+    protected function createOrder($order, $array, $itemQty)
     {
-        try {
-            Mage::register("parentId", $order->getId()); //save orderId
-        }
-        catch (Exception $ex) {
-            echo $ex->getMessage();
-        }
+        $orderParentId = $order->getId();
         foreach($array as $productId)
-        {$store = $order->getStore();
-            $quote = Mage::getModel('sales/quote')->setStoreId($store->getId());
-            $quote->setCurrency($order->AdjustmentAmount->currencyID);
-            $customer = $order->getCustomer();
-            if($customer->getId()==""){
-                //Its guest order
-                $quote->setCheckoutMethod('guest')
-                    ->setCustomerId(null)
-                    ->setCustomerEmail($order->getCustomerEmail())
-                    ->setCustomerIsGuest(true)
-                    ->setCustomerGroupId(Mage_Customer_Model_Group::NOT_LOGGED_IN_ID);
-            } else {
-                // Assign Customer To Sales Order Quote
-                $quote->assignCustomer($customer);
-            }
-            foreach($productId as $id){
-                $product = Mage::getModel('catalog/product')->load($id);
-                $quantity = $itemQty[$id];
-                $quote->addProduct($product,new Varien_Object(array('qty'   => $quantity)));
-            }
-            $billingAddress = $order->getBillingAddress()->getData();
-            $shippingAddress = $order->getShippingAddress()->getData();
-            $billingAddress = $quote->getBillingAddress()->addData($billingAddress);
-            $shippingAddress = $quote->getShippingAddress()->addData($shippingAddress);
-            $paymentMethod = $order->getPayment()->getMethodInstance()->getCode();
-            $shippingAddress->setCollectShippingRates(true)->collectShippingRates()
-                ->setShippingMethod($order->getShippingMethod())
-                ->setPaymentMethod($paymentMethod);
-            // Set Sales Order Payment
-
-            $parentId = Mage::registry('parentId');
-            $quote->getPayment()->importData(array('method' => $paymentMethod));
+        {
+            $quote = $this->assignCustomer($order);
+            $quote = $this->addProduct($productId, $itemQty, $quote);
+            $quote = $this->addBillingShipping($order, $quote);
             $quote->collectTotals()->save();
             $service = Mage::getModel('sales/service_quote', $quote);
             $service->submitAll();
             $sub_order = $service->getOrder();
-            $sub_order->setRelationParentId($parentId);
+            $sub_order->setRelationParentId($orderParentId);
             $sub_order->save();
             $quote->setIsActive(0)->save();
             Mage::getSingleton('checkout/cart')->truncate()->save();
-
-
+            Mage::log($orderParentId, null, 'mylogfile.log');
         }
         try {
-            Mage::register('my_observer_has_run', true);
+            Mage::register(GoldenScent_SplitOrder_Model_Observer::OBSERVER_HAS_RUN, true);
         }
         catch (Exception $ex) {
             echo $ex->getMessage();
         }
 
     }
+
+    protected function addProduct($productId, $itemQty, $quote)
+    {
+        foreach($productId as $id)
+        {
+            $product = Mage::getModel('catalog/product')->load($id);
+            $quantity = $itemQty[$id];
+            $quote->addProduct($product,new Varien_Object(array('qty'   => $quantity)));
+        }
+        return $quote;
+    }
+
+    protected function assignCustomer($order)
+    {
+        $store = $order->getStore();
+        $quote = Mage::getModel('sales/quote')->setStoreId($store->getId());
+        $quote->setCurrency($order->AdjustmentAmount->currencyID);
+        $customer = $order->getCustomer();
+        if($customer->getId()=="")
+        {
+            //Its guest order
+            $quote->setCheckoutMethod('guest')
+                ->setCustomerId(null)
+                ->setCustomerEmail($order->getCustomerEmail())
+                ->setCustomerIsGuest(true)
+                ->setCustomerGroupId(Mage_Customer_Model_Group::NOT_LOGGED_IN_ID);
+        } else {
+            // Assign Customer To Sales Order Quote
+            $quote->assignCustomer($customer);
+        }
+
+        return $quote;
+
+    }
+
+    protected function addBillingShipping($order, $quote)
+    {
+        $billingAddress = $order->getBillingAddress()->getData();
+        $shippingAddress = $order->getShippingAddress()->getData();
+        $billingAddress = $quote->getBillingAddress()->addData($billingAddress);
+        $shippingAddress = $quote->getShippingAddress()->addData($shippingAddress);
+        $paymentMethod = $order->getPayment()->getMethodInstance()->getCode();
+        $shippingAddress->setCollectShippingRates(true)->collectShippingRates()
+            ->setShippingMethod($order->getShippingMethod())
+            ->setPaymentMethod($paymentMethod);
+        // Set Sales Order Payment
+
+        $quote->getPayment()->importData(array('method' => $paymentMethod));
+
+        return $quote;
+    }
+
 
 }
